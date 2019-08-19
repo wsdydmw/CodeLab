ec125a6b1f5fd736ea097a9f6655408abe50cedb
helong.cd <7158518@maodou>
2019-08-19 16:24:28 +0800

    add class load code

--- files/jvm/com/jerry/lab/Test.java
+++ files/jvm/com/jerry/lab/Test.java
@@ -2,6 +2,6 @@ package com.jerry.lab;
 
 public class Test {
     public void hello() {
-        System.out.println("i am loaded by " + getClass().getClassLoader().getClass());
+        System.out.println("i am the most concurrent version class loaded by " + getClass().getClassLoader().getClass());
     }
 }
--- src/main/java/com/jerry/lab/jvm/ClassLoadTest.java
+++ src/main/java/com/jerry/lab/jvm/ClassLoadTest.java
@@ -4,34 +4,49 @@ import java.io.FileInputStream;
 import java.lang.reflect.Method;
 
 /**
- * 使用Thread.setContextClassLoader()切换自定义加载器
+ * 使用自定义加载器，实现类的自定义加载和热部署
  */
 public class ClassLoadTest {
     public static void main(String[] args) throws Exception {
         ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
-        System.out.println("old class loader is " + oldCl);
+        System.out.println("step1 : get old class loader is " + oldCl);
         try {
             Class clazz1 = oldCl.loadClass("com.jerry.lab.Test");
+            System.out.println("step2 : load class com.jerry.lab.Test");
         } catch (ClassNotFoundException e) {
             System.out.println("old class loader can't find com.jerry.lab.Test");
         }
 
-        ClassLoader newCl = new MyClassLoader("F:\\IntelliJ Workspace\\Lab\\JustTest\\external");
-        System.out.println("new class loader is " + newCl);
-        Thread.currentThread().setContextClassLoader(newCl);
-        Class clazz2 = newCl.loadClass("com.jerry.lab.Test");
-        Object obj = clazz2.newInstance();
-        Method helloMethod = clazz2.getDeclaredMethod("hello", null);
-        helloMethod.invoke(obj, null);
+        ClassLoader myCL = new MyClassLoader();
+        System.out.println("step3 : set new class loader " + myCL + " to current thread");
+
+        Class clazz2 = myCL.loadClass("com.jerry.lab.Test");
+        System.out.println("step4 : load class com.jerry.lab.Test");
+
+        Object obj2 = clazz2.newInstance();
+        Method helloMethod2 = clazz2.getDeclaredMethod("hello", null);
+        helloMethod2.invoke(obj2, null);
+
+        myCL = null;
+        clazz2 = null;
+        obj2 = null;
+        helloMethod2 = null;
+        System.gc();
+        System.out.println("unload classed loaded by my class loader");
+
+        Thread.sleep(20 * 1000);//手动修改文件名
+        ClassLoader newCL = new MyClassLoader();
+        Class clazz3 = newCL.loadClass("com.jerry.lab.Test");
+        System.out.println("step5 : reload class com.jerry.lab.Test");
+
+        Object obj3 = clazz3.newInstance();
+        Method helloMethod3 = clazz3.getDeclaredMethod("hello", null);
+        helloMethod3.invoke(obj3, null);
     }
 }
 
 class MyClassLoader extends ClassLoader {
-    private String classPath;
-
-    public MyClassLoader(String classPath) {
-        this.classPath = classPath;
-    }
+    private String classPath = "files\\jvm";
 
     private byte[] loadByte(String name) throws Exception {
         name = name.replaceAll("\\.", "/");
