ec5ec2ad3051686ac6d3c31216bba5865e5321c8
helong.cd <helong.cd@navercorp.com>
2020-03-18 23:56:56 +0800

    add HashMapConcurrentComparer @version 2

--- src/main/java/com/jerry/lab/collection/map/HashMapConcurrentComparer.java
+++ src/main/java/com/jerry/lab/collection/map/HashMapConcurrentComparer.java
@@ -12,32 +12,54 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * write %	HashMap	Hashtable	ConcurrentHashMap	Collections$SynchronizedMap
+ * 0%	1242.0	1451.0	1377.0	1496.0
+ * 2%	1372.0	1471.0	1448.0	1456.0
+ * 4%	1423.0	1555.0	1364.0	1394.0
+ * 6%	1355.0	1431.0	1504.0	1431.0
+ * 8%	1403.0	1566.0	1944.0	1455.0
+ * 10%	1307.0	1388.0	1340.0	1380.0
+ */
 public class HashMapConcurrentComparer {
-    static int DATA_INIT_SIZE = 1000;
-    static int OPERATE_NUM = 500000;
-    static int repeatTime = 2;
+    static int DATA_INIT_SIZE = 2000;
+    static int OPERATE_NUM = 1000000;
+    static int REPEAT_TIME = 1;
+    static int MAX_WRITE_PERCENT = 10;
+    static Map<Integer, Integer>[] targetObjects
+            = new Map[]{new HashMap(), new Hashtable(), new ConcurrentHashMap(), Collections.synchronizedMap(new HashMap<>())};
 
     public static void main(String[] args) {
-        // step1. 准备待测试对象
-        Map<Integer, Integer>[] targetObjects = new Map[]{new HashMap(), new Hashtable(), new ConcurrentHashMap(), Collections.synchronizedMap(new HashMap<>())};
+        new HashMapConcurrentComparer().process();
+        System.exit(0);
+    }
 
-        // step2. 增加重复次数并故意乱序执行顺序
-        List<Map<Integer, Integer>> testObjects = new ArrayList<>();
+    public static int getRandomKey(Map map) {
+        return (int) (Math.random() * map.size());
+    }
+
+    public void process() {
+        // step1. 增加重复次数并故意乱序执行顺序
+        List<Task> Tasks = new ArrayList<>();
         Stream.of(targetObjects).forEach(object -> {
-            for (int i = 1; i <= repeatTime; i++) {
-                testObjects.add(object);
+            for (int writeP = 0; writeP <= MAX_WRITE_PERCENT; writeP += 2) {
+                for (int i = 1; i <= REPEAT_TIME; i++) {
+                    Tasks.add(new Task(object, writeP));
+                }
             }
         });
-        Utils.shuffleList(testObjects);
+        Utils.shuffleList(Tasks);
 
-        // step3. 开始运行
-        Map<Integer, Map> avgResult = new HashMap<>();
-        for (int i = 0; i <= 2; i++) {
-            int writePercent = 0 + 2 * i;
-            avgResult.put(writePercent, new HashMapConcurrentComparer().doTestWithDiffWP(testObjects, writePercent));
-        }
+        // step2. 开始运行
+        List<Result> results = new ArrayList<>();
 
-        // step4. 汇总结果
+        Tasks.stream().forEachOrdered(task -> {
+            Result result = task.process();
+            results.add(result);
+            System.out.println(result);
+        });
+
+        // step3. 汇总结果
         System.out.println("---- 汇总结果 ----");
         // 标题
         System.out.print("write %\t");
@@ -47,99 +69,93 @@ public class HashMapConcurrentComparer {
         System.out.println();
 
         // 每行
-        avgResult.entrySet().stream().forEachOrdered(entry -> {
-            System.out.print(entry.getKey() + "%\t");
-            Map value = entry.getValue();
+        results.stream().collect(Collectors.groupingBy(Result::getWritePercent, Collectors.groupingBy(Result::getObjectName, Collectors.averagingLong(Result::getCostTime))))
+                .entrySet().forEach(entry -> {
+            System.out.print(entry.getKey() + "%\t");//write percent
+            Map value = entry.getValue();//objectName -> costTime
             IntStream.range(0, targetObjects.length).forEachOrdered(index -> {
                 System.out.print(value.get(Utils.getClassName(targetObjects[index])) + "\t");
             });
             System.out.println();
         });
-
-
-        System.exit(0);
     }
 
-    public static int getRandomKey(Map map) {
-        return (int) (Math.random() * map.size());
-    }
+    private class Task {
+        Map<Integer, Integer> map;
+        int writePercent;
 
-    public Map doTestWithDiffWP(List<Map<Integer, Integer>> testObjects, int writePercent) {
-        List<Result> results = new ArrayList<>();
-
-        // step1. 加入计算结果
-        testObjects.stream().forEachOrdered(object -> {
-            Result result = doTestWithDiffObjectAndWP(object, writePercent);
-            results.add(result);
-            System.out.println(result);
-        });
-
-        // step2. 计算结果分析
-        return results.stream().collect(Collectors.groupingBy(Result::getObjectName, Collectors.averagingLong(Result::getCostTime)));
-    }
-
-    public Result doTestWithDiffObjectAndWP(Map<Integer, Integer> map, int writePercent) {
-        Result result = new Result();
-        result.setObjectName(Utils.getClassName(map));
-
-        // 1. init data
-        IntStream.range(0, DATA_INIT_SIZE).forEach(value -> {
-            map.put(value, value);
-        });
-
-        // 2. simulate concurrent
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        int readCount = OPERATE_NUM * (100 - writePercent) / 100;
-        int writeCount = OPERATE_NUM * writePercent / 100;
-        CountDownLatch readCountLatch = new CountDownLatch(readCount);
-        CountDownLatch writeCountLatch = new CountDownLatch(writeCount);
-
-        long begin = System.currentTimeMillis();
-        // read thread
-        new Thread(() -> {
-            for (int i = 0; i < readCount; i++) {
-                executorService.submit(() -> {
-                    int key = getRandomKey(map);
-                    int value = map.containsKey(key) ? map.get(key) : 0;
-                    Utils.calNumber(value);
-                    readCountLatch.countDown();
-                });
-            }
-        }).start();
-
-        // write thread
-        new Thread(() -> {
-            for (int i = 0; i < writeCount; i++) {
-                final int index = i;
-                executorService.submit(() -> {
-                    map.put(DATA_INIT_SIZE + index, DATA_INIT_SIZE + index);
-                    writeCountLatch.countDown();
-                });
-            }
-        }).start();
-
-        try {
-            readCountLatch.await();
-            writeCountLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        public Task(Map<Integer, Integer> map, int writePercent) {
+            this.map = map;
+            this.writePercent = writePercent;
         }
-        result.setCostTime(System.currentTimeMillis() - begin);
 
-        // 3. check result
-        if (DATA_INIT_SIZE + writeCount != map.size()) {
-            result.setSafe(false);
+        public Result process() {
+            Result result = new Result();
+            result.setObjectName(Utils.getClassName(map));
+            result.setWritePercent(writePercent);
+
+            // 1. init data
+            map.clear();
+            IntStream.range(0, DATA_INIT_SIZE).forEach(value -> {
+                map.put(value, value);
+            });
+
+            // 2. simulate concurrent
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            int readCount = OPERATE_NUM * (100 - writePercent) / 100;
+            int writeCount = OPERATE_NUM * writePercent / 100;
+            CountDownLatch readCountLatch = new CountDownLatch(readCount);
+            CountDownLatch writeCountLatch = new CountDownLatch(writeCount);
+
+            long begin = System.currentTimeMillis();
+            // read thread
+            new Thread(() -> {
+                for (int i = 0; i < readCount; i++) {
+                    executorService.submit(() -> {
+                        int key = getRandomKey(map);
+                        int value = map.containsKey(key) ? map.get(key) : 0;
+                        Utils.calNumber(value);
+                        readCountLatch.countDown();
+                    });
+                }
+            }).start();
+
+            // write thread
+            new Thread(() -> {
+                for (int i = 0; i < writeCount; i++) {
+                    final int index = i;
+                    executorService.submit(() -> {
+                        map.put(DATA_INIT_SIZE + index, DATA_INIT_SIZE + index);
+                        writeCountLatch.countDown();
+                    });
+                }
+            }).start();
+
+            try {
+                readCountLatch.await();
+                writeCountLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            result.setCostTime(System.currentTimeMillis() - begin);
+
+            // 3. check result
+            if (DATA_INIT_SIZE + writeCount != map.size()) {
+                result.setSafe(DATA_INIT_SIZE + " + " + writeCount + "!=" + map.size());
+            }
+            return result;
         }
-        return result;
     }
 
     @Data
-    class Result {
+    private class Result {
         String objectName;
+        int writePercent;
         Long costTime;
-        boolean safe = true;
+        String safe = "Safe";
     }
-
 }
+
+
 
 
