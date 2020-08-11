c6d54981c1acb33225ea57174f647adc8564de85
helong06 <helong06@meicai.cn>
2020-08-11 18:47:21 +0800

    线程interrupt方法

--- src/main/java/com/jerry/lab/oo/InterruptTest.java
+++ src/main/java/com/jerry/lab/oo/InterruptTest.java
@@ -3,7 +3,31 @@ package com.jerry.lab.oo;
 public class InterruptTest {
 
     public static void main(String[] args) {
-        Thread t = new Thread(new InterruptableThread());
+        Thread t = new Thread(() -> {
+            int executeTime = 5;
+            while (!Thread.currentThread().interrupted()) {//  while (!Thread.currentThread().interrupted())
+                try {
+                    System.out.println("2. in run() - "
+                            + Thread.currentThread().getName()
+                            + " going to sleep 5 second, now isInterrupted() is "
+                            + Thread.currentThread().isInterrupted());//false
+                    Thread.sleep(5000);
+
+                    System.out.println("7. in run() - "
+                            + Thread.currentThread().getName()
+                            + "woke up");
+
+                } catch (InterruptedException e) {
+                    System.out.println("5. in run() - get InterruptedException， now isInterrupted() is "
+                            + Thread.currentThread().isInterrupted());// false，注意并不是true，抛出异常的时候“是否请求中断标志”被重置了
+                    Thread.currentThread().interrupt();// 并不会立即中断线程，仅仅修改了“是否请求中断标志”为true，如果想停止线程，可在代码中通过检查该标志，自行实现停止逻辑
+                    System.out.println("6. in run() - execute Thread.currentThread().interrupt(), now isInterrupted() is "
+                            + Thread.currentThread().isInterrupted());
+                } finally {
+                    executeTime--;
+                }
+            }
+        });
         t.start();
         System.out.println("1. in main() - begin start thread " + t.getName());
         // 主线程休眠2秒，从而确保刚才启动的线程有机会执行一段时间
@@ -15,31 +39,5 @@ public class InterruptTest {
         System.out.println("3. in main() - interrupting the thread " + t.getName());
         t.interrupt();// 中断线程，导致其抛出异常
         System.out.println("4. in main() - leaving");
-    }
-}
-
-class InterruptableThread implements Runnable {
-
-    public void run() {
-        while (!Thread.currentThread().interrupted()) {
-            try {
-
-                System.out.println("2. in run() - "
-                        + Thread.currentThread().getName()
-                        + " going to sleep 20 second, now isInterrupted() is "
-                        + Thread.currentThread().isInterrupted());//false
-                Thread.sleep(5000);
-                System.out.println("in run() - "
-                        + Thread.currentThread().getName()
-                        + "woke up");
-
-            } catch (InterruptedException e) {
-                System.out.println("5. in run() - get InterruptedException， now isInterrupted() is "
-                        + Thread.currentThread().isInterrupted());// false，并不是true，抛出异常的时候“是否请求中断标志”被重置了
-                Thread.currentThread().interrupt();// 并不会立即中断线程，仅仅修改了“是否请求中断标志”为true，如果想停止线程，可在代码中通过检查该标志，自行实现停止逻辑
-                System.out.println("6. in run() - execute Thread.currentThread().interrupt(), now isInterrupted() is "
-                        + Thread.currentThread().isInterrupted());
-            }
-        }
     }
 }
